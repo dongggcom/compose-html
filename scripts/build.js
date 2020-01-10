ce906597cbb8021ea7d5c4f3acf89a8b1d0ce778
dongmin <simon-dong@qq.com>
2020-01-10 17:51:09 +0800

    定义打包路径

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,12 @@
 const { resolve } = require('path')
-const { writeFile, existsSync, mkdirSync, unlinkSync, rmdirSync } = require('fs')
-const { getRootPath, parseItem, readList } = require('../lib/utils')
+const fs = require('fs')
+const { writeFile, existsSync, mkdirSync, unlinkSync, rmdirSync } = fs
+const { getRootPath, parseItem, memorizeDirReadList } = require('../lib/utils')
 const includeMiddleware  = require('../middleware/include')
 const router = require('../lib/Router')()
 const rootPath = getRootPath()
+
+// include 中间件
 router.registerMiddleware(includeMiddleware)
 
 /**
@@ -17,6 +20,7 @@ const mkDir = function (path, root){
   if (!existsSync(p)) {
     mkdirSync(p)
   }
+  return new Promise((resolve) => resolve())
 }
 
 /**
@@ -24,44 +28,60 @@ const mkDir = function (path, root){
  * @param {string} path 目标文件夹
  * @return {undefined}
  */
-const delDir = function(path){
-  if (path === rootPath) {
-    console.warn("Don't set root path")
-    return;
-  }
-  router.routeAll(path)
-    .then(({ list, dirs }) => {
-      list.map(l => unlinkSync(l))
-      dirs.map(d => rmdirSync(d))
-    }).catch(err=>console.warn(`delete direction in error: ${err}`))
+const delDir = function(path) {
+  return new Promise((resolve, reject) => {
+    if (!existsSync(path)) {
+      return resolve();
+    }
+    if (path === rootPath) {
+      return reject(new Error("Don't set root path"))
+    }
+    return memorizeDirReadList(path)
+      .then(({ list, dirs }) => {
+        list.map(l => unlinkSync(Object.keys(l)[0]))
+        return dirs
+      })
+      .then(dirs => dirs.map(d => rmdirSync(d)))
+      .then(()=>rmdirSync(path))
+      .then(()=>resolve())
+      .catch(err=>console.warn(`delete direction in error: ${err}`))
+  })
 }
 
 module.exports = ({ output, root }) => {
-  // TODO: 删除原打包文件
-  // if( output ){
-  //   delDir(output)
-  // }
-  mkDir(output)
-  router.routeAll(root)
-    .then(({ list, dirs }) => {
-      dirs.forEach(p => mkDir(resolve(output, p.replace(rootPath, './'))))
-      return list.map(v => {
-        const { path } = parseItem(v)
-        const outputPath = resolve(output, path.replace(rootPath, './'))
-        const html = router.render(path)
-        return { [outputPath]: html }
+  const fn = () => 
+    router.routeAll(root)
+      .then(({ list, dirs }) => {
+        const _replaceRootpath = (_path, _rootPath, _output) => resolve(_output, _path.replace(_rootPath, './'))
+        const _root = root || rootPath
+        dirs.forEach(p => mkDir(_replaceRootpath(p, _root, output)))
+        return list.map(v => {
+          const { path } = parseItem(v)
+          const outputPath = _replaceRootpath(path, _root, output)
+          const html = router.render(path)
+          return { [outputPath]: html }
+        })
       })
-    })
-    .then((list) => {
-      list.forEach( async (v)=>{
-        const { path: outputPath, value } = parseItem(v)
-        const html = await value;
-        writeFile(outputPath, html, (err) => {
-          if (err) {
-            console.trace(err)
-          } 
-        })  
-      })
+      .then((list) => {
+        list.forEach( async (v)=>{
+          const { path: outputPath, value } = parseItem(v)
+          const html = await value;
+          writeFile(outputPath, html, (err) => {
+            if (err) {
+              console.trace(err)
+            } 
+          })  
+        })
 
-    }).catch(err=>console.warn(`read direction in error: ${err}`))
+      }).catch(err=>console.warn(`read direction in error: ${err}`))
+  if (output) {
+    async function run(){
+      await delDir(output)
+      await mkDir(output)
+      await fn()
+    }
+    run()
+  } else {
+    fn()
+  }
 }
